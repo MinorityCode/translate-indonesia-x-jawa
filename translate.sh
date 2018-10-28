1208339fd4133240f92cf3f97a635f80a3f92913
MinorityCode <35295801+MinorityCode@users.noreply.github.com>
2018-10-28 02:49:22 -0700

    translate.sh

---
+++ translate.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#####################################################
+#			      Translate Indonesia x Jawa				      #
+#	  Thanks To https://mongosilakan.net for the API	#
+#	           Copyright 2018 - NTB 4 World			    	#
+#	     IG : @minoritycode_ -  FB : Muhammad Fadlan 	#
+#####################################################
+# LOG UPDATE :
+# id-ID&to=jv-NG jv-NG&to=id-ID
+# beta v1.0 : nothing (First Edition)
+# update v1.5 : add input : ./file.sh word
+# update v2.0 : add input  : echo -n "Masukkan kata : "
+# Last update 03/10/2018 - 21:22
+# update v2.0 : add input without +
+# echo "jika ingin terjemahkan kalimat, spasi diganti dengan + (ex : Kamu+cantik)"
+echo -n "Bahasa Indonesia to Jawa? [Y,n] : "
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+    echo -n "Masukkan kata yang ingin di terjemahkan : "
+	read teks
+	# teks=$(echo "$teks" | sed 's/ /+/g');
+	curl=`curl -s "https://mongosilakan.net/api/v1/translate/translate" -H 'Origin: https://mongosilakan.net' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://mongosilakan.net/' -H 'Cookie: __utma=19314980.49552927.1537517823.1537517823.1537517823.1; __utmz=19314980.1537517823.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ga=GA1.2.49552927.1537517823; _gid=GA1.2.1324146535.1537517823; G_ENABLED_IDPS=google; PHPSESSID=ptu2n18q0fggfbefc7c80vv342; _gat=1' -H 'Connection: keep-alive' --data "from=id-ID&to=jv-NG&source=$teks" --compressed`
+	grab=$( echo "$curl" | grep -Po '(?<=basic":")[^"]*' )
+	#echo $curl
+	footer="[MinorityCode_ - Indonesia & Java translate]"
+	#echo $grab
+  if [[ $grab > 0 ]]; then
+    printf "$teks => $grab | $footer.\n"
+fi
+else
+    echo -n "lebokake tembung sing pengen di terjemahake : "
+	read teks
+	# teks=$(echo "$teks" | sed 's/ /+/g');
+	curl=`curl -s "https://mongosilakan.net/api/v1/translate/translate" -H 'Origin: https://mongosilakan.net' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://mongosilakan.net/' -H 'Cookie: __utma=19314980.49552927.1537517823.1537517823.1537517823.1; __utmz=19314980.1537517823.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ga=GA1.2.49552927.1537517823; _gid=GA1.2.1324146535.1537517823; G_ENABLED_IDPS=google; PHPSESSID=ptu2n18q0fggfbefc7c80vv342; _gat=1' -H 'Connection: keep-alive' --data "from=jv-NG&to=id-ID&source=$teks" --compressed`
+	grab=$( echo "$curl" | grep -Po '(?<=basic":")[^"]*' )
+	#echo $curl
+	footer="[MinorityCode_ - Indonesia & Java translate]"
+	#echo $grab
+  if [[ $grab > 0 ]]; then
+    printf "$teks => $grab | $footer.\n"
+fi
+fi
